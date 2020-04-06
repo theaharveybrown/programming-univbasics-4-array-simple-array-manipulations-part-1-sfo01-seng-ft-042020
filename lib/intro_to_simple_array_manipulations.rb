4dc62989596ed58dbb1540a02b6a92f613eebc21
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-06 18:12:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,18 @@
 def using_push(array, string)
+  array.push(string)
+end 
+
+def using_unshift(array, string)
+end 
+
+def using_pop(array)
+end 
+
+def pop_with_arg(array, num)
+end 
+
+def using_shift(array)
+end 
+
+def shift_with_args(array, num)
 end 
